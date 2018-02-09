6aa76e7fba1ba385b3486d2c5c60aecf18184bcf
newhouse <nhitbh@gmail.com>
2018-02-09 22:28:31 +0100

    Added team played games

--- noticias_acb/spiders/acb_spider.py
+++ noticias_acb/spiders/acb_spider.py
@@ -4,16 +4,15 @@ import scrapy
 from noticias_acb.items import TeamItemLoader
 
 
-
 class AcbSpider(scrapy.Spider):
     name = "acb"
     start_urls = ["http://www.acb.com/resulcla.php"]
 
     def parse(self, response):
-        for team in response.css('td.naranja a.negro::text').extract():
-
+        for team in response.css('table.resultados2 tr'):
             item = TeamItemLoader(response=response)
-            item.add_value('name', team)
+            item.add_value('name', team.css('a.negro::text').extract_first())
+            item.add_value('played', team.css('td.grisclaro::text').extract_first())
             yield item.load_item()
             
         
