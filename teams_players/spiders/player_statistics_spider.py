b1d0e9752cc65b58d8c0d94d873e66dd88403d63
newhouse <nhitbh@gmail.com>
2018-02-10 13:33:03 +0100

    Save all stats for each player

--- teams_players/spiders/player_statistics_spider.py
+++ teams_players/spiders/player_statistics_spider.py
@@ -15,24 +15,29 @@ class PlayerStatsSpider(scrapy.Spider):
         tr = response.css('table.estadisticas2')
 
         # stats start at row 2
-        
-        game = tr.css('tr')[2].css('td a::text').extract_first()
-        num_stats = tr.css('tr')[2].css('td::text').extract()
+        game = tr.css('tr')[2:]
+        for td in game:
+            if td is None:
+                continue
+            game = td.css('td a::text').extract_first()
+            if game is None:
+                continue
+            num_stats = td.css('td::text').extract()
 
-        min_t = num_stats[1]
-        pt = num_stats[2]
-        t2 = num_stats[3]
-        t3 = num_stats[4]
-        t1 = num_stats[5]
-        tdo = num_stats[6]
-        a = num_stats[7]
-        br = num_stats[8]
-        c = num_stats[9]
-        fc = num_stats[10]
-        m = num_stats[11]
-        f = num_stats[12]
-        c = num_stats[13]
-        pm = num_stats[14]
-        v = num_stats[15]
+            min_t = num_stats[1]
+            pt = num_stats[2]
+            t2 = num_stats[3]
+            t3 = num_stats[4]
+            t1 = num_stats[5]
+            tdo = num_stats[6]
+            a = num_stats[7]
+            br = num_stats[8]
+            c = num_stats[9]
+            fc = num_stats[10]
+            m = num_stats[11]
+            f = num_stats[12]
+            fpc = num_stats[13]
+            pm = num_stats[14]
+            v = num_stats[15]
 
-        yield {'game' : game, 'min': min_t, 'pt': pt, 't2': t2}
+            yield {'game' : game, 'min': min_t, 'pt': pt, 't2': t2, 't3': t3, 't1': t1, 'tdo': tdo, 'a': a, 'br':br, 'c': c, 'fc': fc, 'm': m, 'f': f, 'fpc': fpc, 'pm': pm, 'v': v}
